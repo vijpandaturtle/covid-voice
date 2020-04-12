023eff8423ce3bfd8f95782beaa3eda49e2d06a1
Bhargava <bganti@magnarus.com>
2020-04-12 14:32:40 +0530

    up files

--- prob_detector/wsgi.py
+++ prob_detector/wsgi.py
@@ -7,19 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prob_detector.settings')
-
-# application = get_wsgi_application()
-
 import os
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prob_detector.settings")
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prob_detector.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
+# import os
+# from django.core.wsgi import get_wsgi_application
+# from whitenoise.django import DjangoWhiteNoise
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prob_detector.settings")
+
+# application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
